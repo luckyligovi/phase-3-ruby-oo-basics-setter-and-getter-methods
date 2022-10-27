bd9801b4614d9330ce1b3654c4817b309fdc3da6
Lucky Ligovi <luckyligovi@gmail.com>
2022-10-27 13:36:08 +0300

    assignment completed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,24 @@
-class Dog
+class Dog 
+    # setter method
+    def name=(name)
+        @name = name
+        
+    end
 
+    # getter method
+    def name
+        @name
+        
+    end
+
+    # setter 
+    def breed=(breed)
+        @breed = breed
+    end
+
+    # getter
+    def breed
+        @breed
+        
+    end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,24 @@
 class Person
+    # getter
+    def name
+        @name
+        
+    end
 
+    # setter
+    def name=(name)
+        @name = name
+        
+    end
+
+    # getter
+    def job
+        @job
+        
+    end
+
+    # setter
+    def job=(job)
+        @job=job
+    end
 end
